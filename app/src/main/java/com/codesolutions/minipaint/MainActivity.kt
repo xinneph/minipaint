446116e12cd1815deaecac089ca17be20093608d
piotr <xinneph@gmail.com>
2021-11-19 11:19:28 +0100

    Displaying the canvas.

--- app/src/main/java/com/codesolutions/minipaint/MainActivity.kt
+++ app/src/main/java/com/codesolutions/minipaint/MainActivity.kt
@@ -2,10 +2,16 @@ package com.codesolutions.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.WindowInsetsController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+//        myCanvasView.windowInsetsController = WindowInsetsController()
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
     }
 }
